92c64b7ef58829b6501fbf7cfdcfac562c012a2d
Luiz Ramos <luizesramos@gmail.com>
2021-01-18 14:13:51 -0500

    Experimenting with routes

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
-if __name__ == "__main__":
+
+@app.route('/')
+def home():
+    return ''
+
+
+@app.route('/raw/<name>')  # route has argument
+def raw(name):
+    return f'<h1>Raw HTML {name}</h1>'
+
+
+@app.route('/raw/')  # ending / is a separate route
+def raw_admin():
+    return redirect(url_for('raw', name='empty'))
+
+
+@app.route('/raw')  # ending in no slash is a third route
+def raw_admin():
+    return redirect(url_for('raw', name='admin'))
+
+
+if __name__ == '__main__':
     app.run()
